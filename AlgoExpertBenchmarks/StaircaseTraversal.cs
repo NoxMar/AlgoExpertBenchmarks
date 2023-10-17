0e41b91c8d519e9d42c6c821ec5b07c8615cb636
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-10-17 09:55:34 +0200

    feat(StaircaseTraversal): add implementation using memoization

--- AlgoExpertBenchmarks/StaircaseTraversal.cs
+++ AlgoExpertBenchmarks/StaircaseTraversal.cs
@@ -14,6 +14,34 @@ public class StaircaseTraversal
         }
         return possibleCombinations;
     }
+    
+    private int StaircaseTraversalMemoize(int height, int maxSteps)
+    {
+        Dictionary<int, int> remainingToWaysCount = new()
+        {
+            [0] = 1,
+            [1] = 1,
+        };
+        return StaircaseTraversalMemoizeHelper(height, maxSteps);
+        int StaircaseTraversalMemoizeHelper(int staircaseHeight, int maxStepsAtATime)
+        {
+            if (remainingToWaysCount.TryGetValue(staircaseHeight, out var result))
+            {
+                return result;
+            }
+            int maxPossibleSteps = Math.Min(staircaseHeight, maxStepsAtATime);
+            int totalWays = 0;
+            for (int steps = 1; steps <= maxPossibleSteps; steps++)
+            {
+                totalWays += remainingToWaysCount.GetValueOrDefault(
+                    staircaseHeight - steps,
+                    StaircaseTraversalMemoizeHelper(staircaseHeight - steps, maxStepsAtATime)
+                );
+            }
+            remainingToWaysCount[staircaseHeight] = totalWays;
+            return totalWays;
+        }
+    }
     [Benchmark]
     public int BasicRecursiveImplementation_6_2()
     {
@@ -23,5 +51,16 @@ public class StaircaseTraversal
     public int BasicRecursiveImplementation_15_5()
     {
         return StaircaseTraversalRecursive(15, 5);
+    }
+    
+    [Benchmark]
+    public int StaircaseTraversal_Memoize_6_2()
+    {
+        return StaircaseTraversalMemoize(6, 2);
+    }
+    [Benchmark]
+    public int StaircaseTraversal_Memoize_15_5()
+    {
+        return StaircaseTraversalMemoize(15, 5);
     }
 }
