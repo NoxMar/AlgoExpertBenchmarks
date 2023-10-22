87c6ac915515ac359765e9ef54b0174d8eb06fb0
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-10-22 23:48:39 +0200

    refactor: make `using`s from BenchmarkDotNet global

--- AlgoExpertBenchmarks/Program.cs
+++ AlgoExpertBenchmarks/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AlgoExpertBenchmarks;
-using BenchmarkDotNet.Running;
-
 BenchmarkRunner.Run<StaircaseTraversal>();
--- AlgoExpertBenchmarks/TournamentWinner.cs
+++ AlgoExpertBenchmarks/TournamentWinner.cs
@@ -1,5 +1,4 @@
 using System.Text;
-using BenchmarkDotNet.Attributes;
 
 namespace AlgoExpertBenchmarks;
 
